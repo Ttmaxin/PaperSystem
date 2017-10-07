d8ca9ffd035479f407d4a482bce5fa0136107931
zengzhuozhen <email@example.com>
2017-10-07 23:48:31 +0800

    code

---
+++ 选项卡demo/src/选项卡demo/Main.java
@@ -0,0 +1,40 @@
+package 选项卡demo;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      // ResultSet rs =sqlQuery.sqlQuery() ;
+		sqlQuery.sqlQuery("1") ;
+       //1.在sqlquery（）中把题目追加到文件，jxxk中就直接显示文件 ✅
+      // 2.数据库的随机选择不重复问题
+       //3.获取用户输入
+       
+//       String job = null;
+//     String id = null;
+//     try {
+//		while(rs.next()){
+//		     //获取stuname这列数据
+//		     job = rs.getString("job");
+//		     //获取stuid这列数据
+//		     id = rs.getString("ename");
+//
+//		     //输出结果
+//		     System.out.println(id + "\t" + job);
+//		 }
+//	} catch (SQLException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//  //   rs.close();
+//		
+     
+    // Jxxk d = new Jxxk("welcome");
+
+	}
+
+}
